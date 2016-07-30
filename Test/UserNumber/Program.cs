19f8abf8e65c327487bfa28f79ddcacc913cdce9
Alexandr Kovalskiy <alex.kowalskiy@gmail.com>
2016-07-30 20:57:03 +0300

    Useer number

---
+++ Test/UserNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserNumber
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string number = Console.ReadLine();
+			char[] a = number.ToArray<char>();
+			Console.Write("the number of numeric in the number: ");
+			Console.WriteLine(a.Length);
+			string[] y = new string[a.Length];
+			int[] z = new int[a.Length];
+			for (int i = 0;i < a.Length;i++)
+			{
+			y[i] = Convert.ToString(a[i]);
+				z[i] = Convert.ToInt32(y[i]);
+			}
+			int value = 0;
+			for (int i = 0;i < z.Length;i++)
+			{
+				value = value + z[i];
+			}
+			Console.WriteLine("The sum of numeric: {0}", value);
+			int arithmetic = value / z.Length;
+			Console.WriteLine("arithmetic mean is {0}", arithmetic);
+			
+			Console.ReadLine();
+		}
+	}
+}
