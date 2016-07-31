88ff1b71d62d5c3bf139c81cf223e5e248f9746a
Alexandr Kovalskiy <alex.kowalskiy@gmail.com>
2016-07-31 14:13:01 +0300

    Add Palindroms program

---
+++ Test/Palindrome/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string number = Console.ReadLine();
+			char[] numberArray = number.ToArray<char>();
+			int countMatches = 0;
+			for (int i = 0;i < numberArray.Length/2;i++)
+			{
+				for (int j = numberArray.Length-1-i;j >=numberArray.Length/2;j--)
+				{
+
+					if (numberArray[i] == numberArray[j])
+					{
+						Console.WriteLine("{0} = {1}", numberArray[i], numberArray[j]);
+
+						countMatches++;
+						break;
+					}
+					
+					
+				}
+			}
+
+			if (countMatches == numberArray.Length / 2)
+			{
+				Console.WriteLine("Number is Palindrome");
+			}
+			else
+			{
+				Console.WriteLine("Number is doesn't palindrome");
+			}
+			Console.ReadLine();
+
+		}
+	}
+}
