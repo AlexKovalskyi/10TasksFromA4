841559e6d28d24ed9157bb72b85a8bddbcbd009f
Alexandr Kovalskiy <alex.kowalskiy@gmail.com>
2016-08-07 18:43:54 +0300

    Aded 1 more variant to palindrom app

--- Test/Reverse/Program.cs
+++ Test/Reverse/Program.cs
@@ -6,20 +6,29 @@ namespace Reverse
 	{
 		static void Main(string[] args)
 		{
-		    
+			Console.WriteLine("Enter number what must be reversed: ");
+			int numberForRevers = Convert.ToInt32(Console.ReadLine());
+			int result = 0;
+			while (numberForRevers != 0)
+			{
+				int remaind;
+				numberForRevers = Math.DivRem(numberForRevers, 10, out remaind);
+				result = result * 10 + remaind;
+			}
+			Console.WriteLine("After revers we take {0}",result);
+			Console.ReadLine();
 		}
-
-	    public static void KoSolution()
-	    {
-            Console.WriteLine("Enter number what must be reversed: "); 
-            string numberForRevers = Console.ReadLine();
-            char[] reversNumber = numberForRevers.ToCharArray();
-            Array.Reverse(reversNumber);
-            Console.Write(reversNumber);
-            Console.ReadKey();
-        }
+		
+		public static void KoSolution()
+		{
+			Console.WriteLine("Enter number what must be reversed: ");
+			string numberForRevers = Console.ReadLine();
+			char[] reversNumber = numberForRevers.ToCharArray();
+			Array.Reverse(reversNumber);
+			Console.Write(reversNumber);
+			Console.ReadKey();
+		}
+		
 	}
-    //TODO: Do this task again in other way without using 'Array.Reverse()'
-
-    //TODO: Do this task again in other way just with digits (for numeric expression).
+	
 }
