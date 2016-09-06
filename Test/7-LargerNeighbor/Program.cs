b383fce05b2c59450e48fbf2ec369990e63ca234
Alexandr Kovalskiy <alex.kowalskiy@gmail.com>
2016-09-06 12:09:58 +0300

    Added task 7 from A4: "LargerNeighbor"

---
+++ Test/7-LargerNeighbor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargerNeighbor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter sequence of number:");
+			string sequence = Console.ReadLine();
+			sequence.ToArray();
+			Console.WriteLine("Large Number from neighbor:");
+			for (int i = 0;i < sequence.Length;i++)
+			{
+				if ( i+2 == sequence.Length)
+				{
+					break;
+				}
+				if (sequence[i] < sequence[i + 1] && sequence[i+1] > sequence[i + 2])
+				{
+					Console.WriteLine(sequence[i + 1]);
+				}
+			}
+			Console.ReadLine();
+		}
+	}
+}
