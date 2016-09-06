401d26e72e3673f99c37099865faec768c98a14a
TommyWhite <moskalets.artem@gmail.com>
2016-09-06 15:32:09 +0300

    Added defensive code block (in case 'sequence' is null or empty).

--- Test/7-LargerNeighbor/Program.cs
+++ Test/7-LargerNeighbor/Program.cs
@@ -6,25 +6,32 @@ namespace LargerNeighbor
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Enter sequence of number (last number must be '0'):");
+			Console.WriteLine("Enter sequence of numbers:");
 			string sequence = Console.ReadLine();
-			string[] arrNumbers = sequence.Split(' ');
-			int[] number = new int[arrNumbers.Length];
-			for (int i = 0;i < arrNumbers.Length - 1;i++)
-			{
-				number[i] = Convert.ToInt32(arrNumbers[i]);
-			}
-			Console.WriteLine("Large Number from neighbor:");
-			for (int pos = 1;pos < number.Length - 1;pos++)
-			{
-				int next = pos + 1;
-				int prev = pos - 1;
-				if (number[prev] < number[pos] && number[pos] > number[next])
-				{
-					Console.WriteLine(number[pos]);
-				}
-			}
-			Console.ReadLine();
+            if (!string.IsNullOrEmpty(sequence))
+		    {
+		        string[] arrNumbers = sequence.Split(' ');
+		        int[] number = new int[arrNumbers.Length];
+		        for (int i = 0 ;i < arrNumbers.Length - 1; i++)
+		        {
+		            number[i] = Convert.ToInt32(arrNumbers[i]);
+		        }
+		        Console.WriteLine("Larger Numbers from neighbors:");
+		        for (int pos = 1; pos < number.Length - 1; pos++)
+		        {
+		            int next = pos + 1;
+		            int prev = pos - 1;
+		            if (number[prev] < number[pos] && number[pos] > number[next])
+		            {
+		                Console.WriteLine(number[pos]);
+		            }
+		        }
+		    }
+		    else
+		    {
+		        Console.WriteLine("Incorrect input.");
+		    }
+		    Console.ReadLine();
 		}
 	}
 }
